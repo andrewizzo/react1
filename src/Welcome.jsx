b881488fd5138703b21d96622e34bca799ce4437
Andrea Izzo <aizzo450@gmail.com>
2023-07-05 22:45:26 +0200

    svolto esercizio 7 react

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,11 +1,11 @@
 import React from "react"; 
 import Age from "./Age";
 
-function Welcome({name=<strong>Andrea</strong>,age="24"}){
+function Welcome({name=<strong>Andrea</strong>,age="4"}){
     return (
         <div>
             <p>Welcome,{name}!</p>
-            <Age age={age}/>
+            {age > 18 ? <Age age={age}/> : <p>it is not greater than 18</p>}
         </div>
     )
 }

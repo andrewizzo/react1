a0454de253b747549f91ce4d5134530c1439ad3d
Andrea Izzo <aizzo450@gmail.com>
2023-07-06 23:21:58 +0200

    svolto esercizio 10 react

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,11 +1,11 @@
 import React from "react"; 
 import Age from "./Age";
 
-function Welcome({name=<strong>Andrea</strong>,age="400"}){
+function Welcome({name="John",age="40"}){
     return (
         <div>
             <p>Welcome,{name}!</p>
-            {age > 18 && age < 65 ? <Age age={age}/> : <p>it is not greater than 18 and less than 65</p>}
+            {age > 18 && age < 65 && name=="John" ? <Age age={age}/> : <p>it is not greater than 18 and less than 65 and the name props is not equal to "John"</p>}
         </div>
     )
 }

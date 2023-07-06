58d98ccf907a89e5ed1312d76d8224962397de8f
Andrea Izzo <aizzo450@gmail.com>
2023-07-06 23:18:17 +0200

    svolto esercizio 9 react

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,14 +1,11 @@
 import React from "react"; 
 import Age from "./Age";
 
-function Welcome({name=<strong>Andrea</strong>,age="455"}){
+function Welcome({name=<strong>Andrea</strong>,age="400"}){
     return (
         <div>
             <p>Welcome,{name}!</p>
-
-            {/* ma non va bene anche per l'esercizio 8? 
-            perchè se la props age c'è mi manda a schermo il componente age,altrimenti mi ritorna il tag p,giusto? */}
-            {age > 18 ? <Age age={age}/> : <p>it is not greater than 18 </p>}
+            {age > 18 && age < 65 ? <Age age={age}/> : <p>it is not greater than 18 and less than 65</p>}
         </div>
     )
 }

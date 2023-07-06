e668f211228c58aad6965f9a21ce88a81398ef03
Andrea Izzo <aizzo450@gmail.com>
2023-07-06 22:51:59 +0200

    svolto esercizio 8 react

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -5,6 +5,9 @@ function Welcome({name=<strong>Andrea</strong>,age="4"}){
     return (
         <div>
             <p>Welcome,{name}!</p>
+
+            {/* ma non va bene anche per l'esercizio 8? 
+            perchè se la props age c'è mi manda a schermo il componente age,altrimenti mi ritorna il tag p,giusto? */}
             {age > 18 ? <Age age={age}/> : <p>it is not greater than 18</p>}
         </div>
     )
